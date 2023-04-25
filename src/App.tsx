95ef5bcfe92de93b56cb9d7e99d1041846aff192
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-04-25 13:25:37 -0500

    Se quita el provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Provider } from "react-redux"
+
 import { Footer } from "./components/Main/Footer"
 import { RouterApp } from "./components/Main/RouterApp"
-import store from './redux/store';
+
 
 function App() {
   
 
   return (
     <>
-      <Provider store={store}>
+      
       <RouterApp/>
       <Footer/>
-      </Provider>
+     
     </>
   )
 }
